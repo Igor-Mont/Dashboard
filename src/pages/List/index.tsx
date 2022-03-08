0af2cbee2b120398190383829ea06cb9ec97f444
Igor Mont <igornathandev@gmail.com>
2022-03-07 21:46:02 -0300

    create filters in List page

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -1,29 +1,50 @@
 import { ContentHeader } from "../../components/ContentHeader";
 import { HistoryFinanceCard } from "../../components/HistoryFinanceCard";
 import { SelectInput } from "../../components/SelectInput";
-import { Container, Content } from "./styles";
+import { Container, Content, Filters } from "./styles";
 
 function List(): JSX.Element {
-  const optionsTest = [
+  const months = [
     {
-      value: 'Igor',
-      label: 'Igor'
+      value: 7,
+      label: 'Julho'
     },
     {
-      value: 'Nathan',
-      label: 'Nathan'
+      value: 8,
+      label: 'Agosto'
     },
     {
-      value: 'Santos',
-      label: 'Santos'
+      value: 9,
+      label: 'Setembro'
+    },
+  ];
+
+  const years = [
+    {
+      value: 2020,
+      label: 2020
+    },
+    {
+      value: 2019,
+      label: 2019
+    },
+    {
+      value: 2018,
+      label: 2018
     },
   ];
 
   return (
     <Container>
       <ContentHeader title="Saídas" lineColor="#E44C4E">
-        <SelectInput options={optionsTest}/>
+        <SelectInput options={months}/>
+        <SelectInput options={years}/>
       </ContentHeader>
+
+      <Filters>
+        <button type="button" className="tag-filter">Recorrentes</button>
+        <button type="button" className="tag-filter">Eventuais</button>
+      </Filters>
 
       <Content>
         <HistoryFinanceCard
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -4,4 +4,42 @@ const Container = styled.div``;
 
 const Content = styled.main``;
 
-export { Container, Content };
+const Filters = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .tag-filter {
+    font-size: 1.2rem;
+    font-weight: 500;
+    background: none;
+
+    color: ${props => props.theme.colors.white};
+
+    margin: 0 0 2.5rem 0.5rem;
+    transition: opacity .3s;
+
+    &:hover {
+      opacity: 0.7;
+    }
+
+    &::after {
+      content: '';
+      display: block;
+      width: 3rem;
+      margin: 0 auto;
+      border-bottom: 10px solid ${props => props.theme.colors.success};
+    }
+
+    &:last-child::after {
+      content: '';
+      display: block;
+      width: 3rem;
+      margin: 0 auto;
+      border-bottom: 10px solid ${props => props.theme.colors.warning};
+    }
+  }
+`;
+
+export { Container, Content, Filters };
